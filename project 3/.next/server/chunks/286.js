3ab4418e60b34780743b026333a554cfd30a1fd9
angusy-56 <aryencken@ucdavis.edu>
2025-07-28 00:08:37 -0700

    Added trace

---
+++ project 3/.next/server/chunks/286.js
@@ -0,0 +1,11 @@
+"use strict";exports.id=286,exports.ids=[286],exports.modules={978:(e,t,d)=>{d.d(t,{Z:()=>r});var p=d(5516);/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,p.Z)("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]])},1737:(e,t,d)=>{d.d(t,{Z:()=>r});var p=d(5516);/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,p.Z)("CircleCheckBig",[["path",{d:"M21.801 10A10 10 0 1 1 17 3.335",key:"yps3ct"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]])}};
---
+++ project 3/.next/server/chunks/401.js
@@ -0,0 +1,16 @@
+"use strict";exports.id=401,exports.ids=[401],exports.modules={978:(e,r,t)=>{t.d(r,{Z:()=>d});var y=t(5516);/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let d=(0,y.Z)("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]])},6542:(e,r,t)=>{t.d(r,{Z:()=>d});var y=t(5516);/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let d=(0,y.Z)("CircleX",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]])},7927:(e,r,t)=>{t.d(r,{Z:()=>d});var y=t(5516);/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let d=(0,y.Z)("CreditCard",[["rect",{width:"20",height:"14",x:"2",y:"5",rx:"2",key:"ynyp8z"}],["line",{x1:"2",x2:"22",y1:"10",y2:"10",key:"1b3vmo"}]])}};
